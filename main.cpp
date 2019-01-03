37278107f3e1e83b54f48ffd7f052da4116bb58d
Rares Dumitrica <raresdumitrica@yahoo.com>
2019-01-03 23:48:13 +0200

    al 2 lea main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-include <iostream>
+#include <iostream>
 #include<typeinfo>
 #include "angajat.h"
 
@@ -6,9 +6,11 @@ using namespace std;
 
 int main()
 {
-    angajat <double> ob1("Alin", 20.35), ob2, ob3;
-    ob2 = ob3 = ob1;
-    //cout << ob1 << endl << ob2 << endl << ob3;
-   ob1.afisare();
-ob2.afisare();}
+    angajat <double> ob1("Alin", 20.35), ob2("Vici", 2.3211), ob3;
+    ob3 = ob2;
+    cout << ob1 << endl << ob2 << endl << ob3;
+    //ob1.afisare;
 
+    administrator <int> ob4;
+    cin >> ob4;
+}
