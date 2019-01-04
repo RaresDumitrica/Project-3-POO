570086b5bae910588e5140d285f748b25779df3f
Rares Dumitrica <raresdumitrica@yahoo.com>
2019-01-05 01:59:10 +0200

    main -ul

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include<typeinfo>
 #include "angajat.h"
+#include "administrator.h"
+#include "manager.h"
+#include "directorExecutiv.h"
 
 using namespace std;
 
 int main()
 {
-    angajat <double> ob1("Alin", 20.35), ob2("Vici", 2.3211), ob3;
+    /*angajat <double> ob1("Alin", 20.35), ob2("Vici", 2.3211), ob3;
     ob3 = ob2;
     cout << ob1 << endl << ob2 << endl << ob3;
-    //ob1.afisare;
+    //ob1.afisare;*/
 
-    administrator <int> ob4;
-    cin >> ob4;
+    //administrator <double> ob4("Victoria", 54.25, 4);
+    //cin >> ob4;
+    //cout << ob4;
+    //ob4.afisare();
+
+    manager <double> ob5;
+    cin>>ob5;
+    cout<<ob5;
+
+    //directorExecutiv <double> ob6;
+   // cin>>ob6;
+   // cout<<ob6;
 }
